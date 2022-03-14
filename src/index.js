146dd6c17ecbccf5e30bf096105390e1fa21a39a
Brandon <101501592+brandonbanuelos@users.noreply.github.com>
2022-03-13 19:43:43 -0700

    Remove hash router

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter> from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
