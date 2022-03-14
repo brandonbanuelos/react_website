dcb3f5194ce3d56cb89c5ad2d3a7ff643793136b
Brandon <101501592+brandonbanuelos@users.noreply.github.com>
2022-03-13 19:52:29 -0700

    Add slash

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   
   return (
     <div className="App">
-    <BrowserRouter basename={PUBLIC_URL}>
+    <BrowserRouter basename={PUBLIC_URL +'/'}>
       <Navbar />
       <Routes>
         <Route path= '/' element={<Home />} />
