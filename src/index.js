a3751c39ca4359fd9e10466b5484ef2f25202098
Brandon <101501592+brandonbanuelos@users.noreply.github.com>
2022-03-13 18:40:07 -0700

    Add hashrouter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter> from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
