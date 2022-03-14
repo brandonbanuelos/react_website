471e42152630b5dc1511602d38c1b520c58c73ae
Brandon <101501592+brandonbanuelos@users.noreply.github.com>
2022-03-13 18:40:39 -0700

    Remove hashrouter

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Router basename={process.env.PUBLIC_URL}>
-      <Navbar />
-      <Routes>
-        <Route exact path= '/' element={<Home />} />
-        <Route exact path= '/projects' element={<Projects />} />
-        <Route exact path= '/about' element={<About />} />
-      </Routes>
-      <Footer />
-    </Router>
+    
+    <Navbar />
+    <Routes>
+      <Route exact path= '/' element={<Home />} />
+      <Route exact path= '/projects' element={<Projects />} />
+      <Route exact path= '/about' element={<About />} />
+    </Routes>
+    <Footer />
+    
     </div>
   );
 }
