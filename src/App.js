48f3637cc6ebf1ad32f28cbc342e152ff0c9aacd
Brandon <101501592+brandonbanuelos@users.noreply.github.com>
2022-03-13 19:54:21 -0700

    Add console log

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter basename={PUBLIC_URL +'/'}>
+      console.log(PUBLIC_URL)
       <Navbar />
       <Routes>
         <Route path= '/' element={<Home />} />
